fd530bd4b36e0f0b971095fb1d02dd1d49a90581
Metaspolit01 <kandykarthik4@gmail.com>
2025-07-03 09:49:18 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -26,6 +26,10 @@ def hello():
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
 
+@app.route('/health')
+def health():
+    return 'OK', 200
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
